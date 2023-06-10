228e1b33fce4bf36695225c6f8414c63f0a65390
Jay <jayagra@cisco.com>
2023-06-10 18:34:00 +0530

    base working without proper css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
 
 function App() {
+  const [activeUserIndex, setActiveUserIndex] = useState(0);
+  const [usersList, setUsersList] = useState([
+    "Taylor",
+    "Alexa",
+    "Srinivasa",
+    "Atlantas",
+  ]);
+  const [showAdd, setShowAdd] = useState(false);
+  const [newUser, setNewUser] = useState("");
+
+  const handleOnClick = (e) => {
+    let id = e.target.id;
+    setActiveUserIndex(id);
+  };
+
+  const handleOnDelete = (e) => {
+    let id = Number(e.currentTarget.id);
+    let newUsersList = [...usersList];
+    newUsersList = newUsersList.filter((user, index) => index !== id);
+    setUsersList(newUsersList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="buttons">
+        {usersList.map((user, index) => {
+          return (
+            <div className="button-row" key={index}>
+              <button id={index} onClick={handleOnClick}>
+                {user}
+              </button>
+              <DeleteRoundedIcon id={index} onClick={handleOnDelete} />
+            </div>
+          );
+        })}
+        <br />
+        <button
+          onClick={() => {
+            setShowAdd(true);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          ADD USER
+        </button>
+        {showAdd && (
+          <>
+            <input
+              onChange={(e) => {
+                setNewUser(e.target.value);
+              }}
+              value={newUser}
+            ></input>
+            <button
+              onClick={() => {
+                setNewUser("");
+                setUsersList([...usersList, newUser]);
+                setShowAdd(false);
+              }}
+            >
+              ADD
+            </button>
+          </>
+        )}
+      </div>
+      <div className="input">
+        <textarea></textarea>
+        <button>Send mail to {usersList[activeUserIndex]}</button>
+      </div>
     </div>
   );
 }
